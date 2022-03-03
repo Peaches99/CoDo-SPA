268c74d7962ad676ed71ccac6542b9d6c63bb697
Kieran Kirchweger <kir19567@spengergasse.at>
2022-03-03 09:06:38 +0100

    Color scheme created

--- lib/util/globalTheme.dart
+++ lib/util/globalTheme.dart
@@ -1,1 +1,23 @@
 //TODO: Puuuul mach colour scheme
+import 'package:flutter/material.dart';
+
+class GlobalTheme {
+  static ThemeData bitlogTheme() {
+    return ThemeData(
+      colorScheme: const ColorScheme(
+        primary: Color(0xff3C1B43),
+        secondary: Color(0xff501537),
+        tertiary: Color(0xff922D50),
+        brightness: Brightness.dark,
+        background: Color(0xff4F7CAC),
+        error: Colors.red,
+        onPrimary: Colors.white,
+        onBackground: Colors.white,
+        onError: Colors.black,
+        onSecondary: Colors.white,
+        onSurface: Colors.white,
+        surface: Color(0xffC0E0DE)
+      )
+    );
+  }
+}
